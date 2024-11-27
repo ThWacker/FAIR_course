91b3ef70c2c2f0bbadbbac1ce09ec3e944a3da9e
Theresa Wacker <theresa_wacker@mailbox.org>
2024-11-27 11:33:31 +0000

    added comments

--- astronaut-data-analysis-not-so-fair-main/eva_data_analysis.py
+++ astronaut-data-analysis-not-so-fair-main/eva_data_analysis.py
@@ -6,18 +6,29 @@ input_json = open('eva-data.json', 'r')
 spacewalks_csv = open('eva-data.csv','w')
 spacewalks_plot = 'myplot.png'
 
-fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
+print("--START--")
+print(f"Reading JSON file {input_json}")
+
+# Read the data from the JSON using pandas
 eva_df= pd.read_json(input_json, convert_dates=['date'])
 eva_df['eva']=eva_df['eva'].astype(float)
+#Clean the data by removing incomplete rows and sort it by date
 eva_df.dropna(axis=0, inplace=True)
 eva_df.sort_values('date', inplace=True)
 
+print(f"Saving to CSV file {spacewalks_csv}")
+#Save dataframe to csv for later analysis
 eva_df.to_csv(spacewalks_csv, index=False)
 
+print(f"Plotting cumulative spacewalk duration and save to {spacewalks_plot}")
+
+# Get the duration in hours
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+#add cumulative time column 
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 
+# plotting 
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
@@ -25,3 +36,4 @@ plt.tight_layout()
 plt.savefig(spacewalks_plot)
 plt.show()
 
+print("--END--")
