7679b5d530e705a952e4131dfd5aef5e64be1fdb
Theresa Wacker <theresa_wacker@mailbox.org>
2024-11-28 11:38:15 +0000

    added additional functions

--- astronaut-data-analysis-not-so-fair-main/eva_data_analysis.py
+++ astronaut-data-analysis-not-so-fair-main/eva_data_analysis.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re  # added this line
+
 
 def main(input_file, output_file, graph_file):
     print("--START--")
 
     eva_data = read_json_to_dataframe(input_file)
 
+    eva_data = add_crew_size_column(eva_data)  # added this line
+
     write_dataframe_to_csv(eva_data, output_file)
 
     plot_cumulative_time_in_space(eva_data, graph_file)
 
     print("--END--")
+
 
 def read_json_to_dataframe(input_file):
     """
@@ -24,11 +29,11 @@ def read_json_to_dataframe(input_file):
     Returns:
          eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
     """
-    print(f'Reading JSON file {input_file}')
-    eva_df = pd.read_json(input_file, convert_dates=['date'])
-    eva_df['eva'] = eva_df['eva'].astype(float)
+    print(f"Reading JSON file {input_file}")
+    eva_df = pd.read_json(input_file, convert_dates=["date"])
+    eva_df["eva"] = eva_df["eva"].astype(float)
     eva_df.dropna(axis=0, inplace=True)
-    eva_df.sort_values('date', inplace=True)
+    eva_df.sort_values("date", inplace=True)
     return eva_df
 
 
@@ -43,8 +48,9 @@ def write_dataframe_to_csv(df, output_file):
     Returns:
         None
     """
-    print(f'Saving to CSV file {output_file}')
+    print(f"Saving to CSV file {output_file}")
     df.to_csv(output_file, index=False)
+
 
 def text_to_duration(duration):
     """
@@ -57,7 +63,9 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/60  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = (
+        int(hours) + int(minutes) / 60
+    )  # there is an intentional bug on this line (should divide by 60 not 6)
     return duration_hours
 
 
@@ -72,9 +80,7 @@ def add_duration_hours_variable(df):
         df_copy (pd.DataFrame): A copy of df_ with the new duration_hours variable added
     """
     df_copy = df.copy()
-    df_copy["duration_hours"] = df_copy["duration"].apply(
-        text_to_duration
-    )
+    df_copy["duration_hours"] = df_copy["duration"].apply(text_to_duration)
     return df_copy
 
 
@@ -94,27 +100,59 @@ def plot_cumulative_time_in_space(df, graph_file):
     Returns:
         None
     """
-    print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
+    print(f"Plotting cumulative spacewalk duration and saving to {graph_file}")
     df = add_duration_hours_variable(df)
-    df['cumulative_time'] = df['duration_hours'].cumsum()
-    plt.plot(df.date, df.cumulative_time, 'ko-')
-    plt.xlabel('Year')
-    plt.ylabel('Total time spent in space to date (hours)')
+    df["cumulative_time"] = df["duration_hours"].cumsum()
+    plt.plot(df.date, df.cumulative_time, "ko-")
+    plt.xlabel("Year")
+    plt.ylabel("Total time spent in space to date (hours)")
     plt.tight_layout()
     plt.savefig(graph_file)
     plt.show()
 
 
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        int: The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r";", crew)) - 1
+
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of df with the new crew_size variable added
+    """
+    print("Adding crew size variable (crew_size) to dataset")
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(calculate_crew_size)
+    return df_copy
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        input_file = './data/eva-data.json'
-        output_file = './results/eva-data.csv'
-        print(f'Using default input and output filenames')
+        input_file = "./data/eva-data.json"
+        output_file = "./results/eva-data.csv"
+        print(f"Using default input and output filenames")
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
-        print('Using custom input and output filenames')
+        print("Using custom input and output filenames")
 
-    graph_file = './results/cumulative_eva_graph.png'
+    graph_file = "./results/cumulative_eva_graph.png"
     main(input_file, output_file, graph_file)
